9b4d3aca91ebbada757e1736705c0e0a8126e79d
Ubuntu <ubuntu@ip-172-31-7-23.us-east-2.compute.internal>
2024-04-27 21:23:04 +0000

    chat

--- flask_app/run.py
+++ flask_app/run.py
@@ -9,12 +9,9 @@ socketio = SocketIO(app)
 
 game_engine = GameEngine()
 
-async def call_llm():
-    game_engine.add_message("I AM A LLM")
-    send("I AM A LLM", broadcast=True)
-
 @app.route("/")
 def index():
+    print(io.sockets.clients();)
     html_messages = ""
     for message in game_engine.messages:
         html_messages += '<div class="p-2 mb-2 bg-secondary text-white rounded">' + message + '</div>'
@@ -26,9 +23,10 @@ def handle_message(message):
     send(message, broadcast=True)
 
     if len(game_engine.messages) % 3 == 0:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.create_task(call_llm())
+        game_engine.add_message("I AM A LLM")
+        send("I AM A LLM", broadcast=True)
+        print("Receied")
+        
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', debug=True)
