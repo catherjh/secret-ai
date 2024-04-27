2066dadd87a99a95d343a80d953342f12c2fd7e9
Ubuntu <ubuntu@ip-172-31-7-23.us-east-2.compute.internal>
2024-04-27 22:10:47 +0000

    Change emit location

--- flask_app/engine.py
+++ flask_app/engine.py
@@ -3,9 +3,11 @@ class GameEngine:
         self.prompts = ["You are all on a remote planet full of weird creatures and animals. Feel free to create scenarios and animals as you all traverse together across this foreign landscape."]
         self.curr_prompt = self.prompts[0]
         self.messages = []
+        self.users = []
 
     def reset(self):
         self.messages = []
+        self.users = []
 
     def add_message(self, msg):
         self.messages.append(msg)
--- flask_app/run.py
+++ flask_app/run.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit
 from engine import GameEngine
 import asyncio
+import string
+import random 
+
 
 from gateways.openai_gateway import OpenAIGateway
 
@@ -15,27 +18,31 @@ openai_gateway = OpenAIGateway()
 
 @app.route("/")
 def index():
+    if "user" not in session or session["user"] not in game_engine.users:
+        username = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+        session["user"] = username
+        game_engine.users.append(username)
+
     html_messages = ""
-    for message in game_engine.messages:
-        html_messages += (
-            '<div class="p-2 mb-2 bg-secondary text-white rounded">'
-            + message
-            + "</div>"
-        )
-    return render_template("index.html", html_messages=html_messages)
+    for message_json in game_engine.messages:
+        html_messages += f'<div class="p-2 mb-2 bg-secondary text-white rounded">{message_json["user"]}: {message_json["msg"]}</div>'
+    return render_template('index.html', html_messages=html_messages)
 
 
-@socketio.on("message")
+@socketio.on("chat")
 def handle_message(message):
-    game_engine.add_message(message)
-    send(message, broadcast=True)
+    if "user" not in session or session["user"] not in game_engine.users:
+        return 
+     
+    game_engine.add_message({"msg": message, "user": session["user"]})
+    emit("chat", session["user"] + ": " + message, broadcast=True)
 
     if len(game_engine.messages) % 3 == 0:
         # TODO: store previous messages as dicts in the game engine instead of converting here
         previous_messages = [{"message": m} for m in game_engine.messages]
         msg = openai_gateway.chat_completion(previous_messages)
         game_engine.add_message(msg)
-        send(msg, broadcast=True)
+        emit("chat", ("AI", msg), broadcast=True)
         print("Received")
 
 
